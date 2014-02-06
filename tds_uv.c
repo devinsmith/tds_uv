c6b19d8a0fab9510827c75464f5de98f08414da6
Devin Smith <devin@devinsmith.net>
2014-02-05 22:22:56 -0800

    Add connection stuff.

--- tds_uv.c
+++ tds_uv.c
@@ -6,17 +6,43 @@
 uv_loop_t *loop;
 
 static void
+on_connect(uv_connect_t *req, int status)
+{
+	if (status == -1) {
+		fprintf(stderr, "connect failed error %s\n", uv_strerror(status));
+		free(req);
+		return;
+	}
+
+	free(req);
+}
+
+
+static void
 on_resolved(uv_getaddrinfo_t *resolver, int status, struct addrinfo *res)
 {
 	char addr[17];
+	uv_connect_t *connect_req;
+	uv_tcp_t *socket;
+
 	if (status < 0) {
-		fprintf(stderr, "getaddrinfo callback error\n");
+		fprintf(stderr, "getaddrinfo callback error: %s\n", uv_strerror(status));
 		return;
 	}
 
 	addr[16] = '\0';
 	uv_ip4_name((struct sockaddr_in*) res->ai_addr, addr, 16);
 	fprintf(stderr, "%s\n", addr);
+
+	connect_req = (uv_connect_t *)malloc(sizeof(uv_connect_t));
+	socket = (uv_tcp_t *)malloc(sizeof(uv_tcp_t));
+	uv_tcp_init(loop, socket);
+
+	connect_req->data = (void*) socket;
+	uv_tcp_connect(connect_req, socket, res->ai_addr,
+	    on_connect);
+
+	uv_freeaddrinfo(res);
 }
 
 int
