e589be6cb218e4735cf4f685866b146b9cbf7803
Devin Smith <devin@devinsmith.net>
2014-02-10 16:47:13 -0800

    Parse response from SQL RP.

--- sqlrp.c
+++ sqlrp.c
@@ -30,10 +30,20 @@
 /* Message types ([MC-SQLR].pdf section 2.2) */
 #define CLNT_UCAST_INST 0x04
 
+#ifdef WIN32
+#define strcasecmp _stricmp
+#endif
+
 static void
 sqlrp_on_read(uv_udp_t *handle, ssize_t nread, const uv_buf_t *buf,
     const struct sockaddr *addr, unsigned flags)
 {
+	char *p;
+	struct connection *conn = handle->data;
+	int port_found = 0;
+	int instance_found = 0;
+	long l = 0;
+
 	if (nread < 0) {
 		fprintf(stderr, "detect port read error\n");
 		/* Error or EOF */
@@ -54,8 +64,55 @@ sqlrp_on_read(uv_udp_t *handle, ssize_t nread, const uv_buf_t *buf,
 	}
 
 	fprintf(stderr, "%d bytes read\n", (int)nread);
-	/* TODO: Handle */
+	buf->base[nread - 1] = '\0';
 	dump_hex(buf->base, nread);
+	/* TODO: Handle */
+	if (buf->base[0] != 5) {
+		fprintf(stderr, "invalid packet received\n");
+		goto done;
+	}
+	fprintf(stderr, "%s\n", conn->instance);
+
+	p = buf->base + 3; /* Skip packet type + length */
+	while (*p) {
+		char *name, *value;
+
+		name = p;
+		p = strchr(p, ';');
+		if (!p)
+			break;
+
+		*p++ = '\0';
+
+		value = name;
+		if (*name) {
+			value = p;
+			p = strchr(p, ';');
+			if (!p)
+				break;
+			*p++ = '\0';
+		}
+
+		if (strcasecmp(name, "InstanceName") == 0) {
+			if (strcasecmp(value, conn->instance))
+				break;
+			instance_found = 1;
+		} else if (strcasecmp(name, "tcp") == 0) {
+
+			l = strtol(value, &p, 10);
+			if (l > 0 && l <= 0xffff && *p == 0)
+				port_found = 1;
+		}
+	}
+	if (instance_found && port_found) {
+		struct sockaddr_in addr;
+		conn->port = l;
+		fprintf(stderr, "Port = %d\n", conn->port);
+		uv_ip4_addr(conn->ip_addr, conn->port, &addr);
+		tds_connect(conn, (struct sockaddr *)&addr);
+	}
+
+done:
 	free(buf->base);
 	uv_udp_recv_stop(handle);
 	free(handle);
@@ -90,6 +147,7 @@ sqlrp_detect_port(uv_loop_t *loop, struct connection *conn)
 
 	/* Setup our sending UDP datagram */
 	send_socket = malloc(sizeof(uv_udp_t));
+	send_socket->data = conn;
 
 	uv_udp_init(loop, send_socket);
 	fprintf(stderr, "Sending to %s:%d\n", conn->ip_addr, SQLRP_PORT);
@@ -104,7 +162,6 @@ sqlrp_detect_port(uv_loop_t *loop, struct connection *conn)
 	buffer.base = (char *)msg;
 
 	send_req = malloc(sizeof(uv_udp_send_t));
-	send_req->data = conn;
 
 	uv_ip4_addr(conn->ip_addr, SQLRP_PORT, &send_addr);
 	uv_udp_send(send_req, send_socket, &buffer, 1,
@@ -112,3 +169,4 @@ sqlrp_detect_port(uv_loop_t *loop, struct connection *conn)
 
 	return 0;
 }
+
